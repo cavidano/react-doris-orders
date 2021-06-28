7f0b0f2c372963fa658b94dea732d2c17777ce7e
Carl Avidano <cavidano@gmail.com>
2021-06-28 09:06:40 -0400

    Update index.js

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+
     render() {
+
+        state = {
+            empty: true
+        }
+        
         return (
             <form className="needs-validation" action="" noValidate>
 
