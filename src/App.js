8dc84bc69b39b3faff9c924a9ef2718b2f288320
Carl Avidano <cavidano@gmail.com>
2021-07-19 04:28:43 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,44 +21,65 @@ class App extends Component {
   render(){
     
     return (
-      <div className="App">
-
-        <Header agencyName="Department of Records" />
-      
-        <main id="skip-header-content">
-
-          <PageTitle title="Order Vital Records" />
-
-          <div className="container-fluid wide">
-
-              <div className="row">
-
-                  <div className="col-lg-9 overflow-hidden" id="primary-content">
-
-                      <Form />
-
-                  </div>
-                      
-                  <div className="col-lg-3 border-left">
-                      
-                      <div className="sticky-top py-2">
-
-                          <OrderSidebar />
-
-                      </div>
-                      
-                  </div>
-                  
-              </div>
-              
-          </div>
-          
-        </main>
-
-        <Footer />
+      <Router>
         
-      </div>
-    );
+        <div className="App">
+
+          <Header agencyName="Department of Records" />
+        
+          <main id="skip-header-content">
+
+            <Switch>
+
+              <Route exact path="/" render={(props) => (
+
+              <Fragment>
+
+                <PageTitle title="Order Vital Records" />
+
+                <div className="container-fluid wide">
+
+                    <div className="row">
+
+                        <div className="col-lg-9 overflow-hidden" id="primary-content">
+
+                            <Form />
+
+                        </div>
+                            
+                        <div className="col-lg-3 border-left">
+                            
+                            <div className="sticky-top py-2">
+
+                                <OrderSidebar />
+
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+              
+              </Fragment>
+
+              )} />
+            
+              <Route exact path="/cart" render={(props) => (
+                
+                <PageTitle title="Cool Cart" />
+
+              )} />
+              
+            </Switch>
+
+          </main>
+
+          <Footer />
+          
+        </div>
+    
+      </Router>);
   
   };
 
