583d91f6735c026886c33c74f232c7633ec46baf
Carl Avidano <cavidano@gmail.com>
2021-07-19 04:13:40 -0400

    before adding react router dom

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 // Components
 
 import Header from './components/Header';
@@ -9,47 +12,56 @@ import OrderSidebar from './components/OrderDetails/OrderSidebar';
 // Global CSS File
 import './theme.css';
 
-function App() {
+class App extends Component {
 
-  return (
-    <div className="App">
+  async componentDidMount() {
 
-      <Header agencyName="Department of Records" />
+  }
+
+  render(){
     
-      <main id="skip-header-content">
+    return (
+      <div className="App">
 
-        <PageTitle title="Order Vital Records" />
-
-        <div className="container-fluid wide">
-
-            <div className="row">
-
-                <div className="col-lg-9 overflow-hidden" id="primary-content">
-
-                    <Form />
-
-                </div>
-                    
-                <div className="col-lg-3 border-left">
-                    
-                    <div className="sticky-top py-2">
-
-                        <OrderSidebar />
-
-                    </div>
-                    
-                </div>
-                
-            </div>
-            
-        </div>
-        
-      </main>
-
-      <Footer />
+        <Header agencyName="Department of Records" />
       
-    </div>
-  );
+        <main id="skip-header-content">
+
+          <PageTitle title="Order Vital Records" />
+
+          <div className="container-fluid wide">
+
+              <div className="row">
+
+                  <div className="col-lg-9 overflow-hidden" id="primary-content">
+
+                      <Form />
+
+                  </div>
+                      
+                  <div className="col-lg-3 border-left">
+                      
+                      <div className="sticky-top py-2">
+
+                          <OrderSidebar />
+
+                      </div>
+                      
+                  </div>
+                  
+              </div>
+              
+          </div>
+          
+        </main>
+
+        <Footer />
+        
+      </div>
+    );
+  
+  };
+
 }
 
 export default App;
